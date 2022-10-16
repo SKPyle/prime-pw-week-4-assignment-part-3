ba9b42ba23d467cfbf3ab19485dfe7a89641e6f3
Seth Pyle <sethpyle@yahoo.com>
2022-10-16 16:32:03 -0500

    completed last two functions before the stretch section

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,29 +3,38 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = ['cherries','apples','bagels'];
 
+
+
 function addItem(item){
-if (basket.push()){
-    return "true";
-}
-    return "false";
-}
+    if (basket.push('')){
+        return "true";
+    }
+        return "false";
+    }
+    
 console.log(addItem());
 
+
 function listItems(){
-for(let i = 0; i < basket.length; i++){
+for (let i = 0; i < basket.length; i++){
     console.log(basket[i]);
 }
 }
-
 console.log(listItems());
 
 
-function empty(){
-
-    
+function empty() {
+if(basket.length > 0){
+   return basket = [];
+  }
+  return "add items to basket"
 }
 
-/* Create a function called `empty`. It should:
+console.log(empty());
+ 
+
+
+  /* Create a function called `empty`. It should:
   - reset the `basket` to an empty array
 
 */
