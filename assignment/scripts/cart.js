e89cd525cae348bdae1d46dfba8931b01847cb21
Seth Pyle <sethpyle@yahoo.com>
2022-10-16 15:29:39 -0500

    created first two functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,31 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = ['cherries','apples','bagels'];
+
+function addItem(item){
+if (basket.push()){
+    return "true";
+}
+    return "false";
+}
+console.log(addItem());
+
+function listItems(){
+for(let i = 0; i < basket.length; i++){
+    console.log(basket[i]);
+}
+}
+
+console.log(listItems());
+
+
+function empty(){
+
+    
+}
+
+/* Create a function called `empty`. It should:
+  - reset the `basket` to an empty array
+
+*/
